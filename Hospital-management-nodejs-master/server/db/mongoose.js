438e2d77749519cffa251f805d04adfdfc831e15
ravi-g-786 <128693934+ravi-g-786@users.noreply.github.com>
2023-08-14 20:47:49 +0530

    Update mongoose.js

--- Hospital-management-nodejs-master/server/db/mongoose.js
+++ Hospital-management-nodejs-master/server/db/mongoose.js
@@ -4,6 +4,6 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
+mongoose.connect("mongodb+srv://ravimarolix123:<password>@cluster0.ghioxgn.mongodb.net/?retryWrites=true&w=majority");
 
 module.exports = {mongoose};
